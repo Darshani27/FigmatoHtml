c019678df74102d7fe5c7f278e2d6011d6fa4f44
Darshani27 <80616303+Darshani27@users.noreply.github.com>
2023-01-27 16:13:41 +0530

    Update mat.module.ts

--- mat.module.ts
+++ mat.module.ts
@@ -1,20 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 
 
-
-
-
-
-const modules=[MatButtonModule];
-
+const modules=[MatButtonModule,MatIconModule];
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ],
-  exports:[modules]
+    CommonModule],
+    exports:[modules]
 })
 export class MatModule { }
